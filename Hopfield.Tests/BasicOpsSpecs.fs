0e36d5cb6a012208a5391f30369eec812d9ee2c5
Chris Bilson <cbilson@pobox.com>
2009-02-04 05:42:32 -0800

    initial commit

---
+++ Hopfield.Tests/BasicOpsSpecs.fs
@@ -0,0 +1,59 @@
+﻿#light
+
+open Microsoft.FSharp.Math
+open Xunit
+open FsxUnit.Syntax
+open Ops
+
+[<Fact>]
+let it_converts_true_to_bipolar_1 =
+    toBipolar true |> should equal 1.0
+
+[<Fact>]
+let it_converts_false_to_bipolar_negative_1 =
+    toBipolar false |> should equal -1.0
+
+[<Fact>]
+let it_converts_1_to_boolean_true = 
+    toBoolean 1.0 |> should equal true
+    
+[<Fact>]
+let it_converts_0_to_boolean_false = 
+    toBoolean 0.0 |> should equal false
+    
+[<Fact>]
+let it_converts_boolean_matrices_to_bipolar =
+    let m = Matrix.Generic.of_list [[ false; true; false; true ]]
+    toBipolarM m |> should equal (matrix [[ -1.0; 1.0; -1.0; 1.0 ]])
+
+[<Fact>]
+let it_converts_bipolar_matrices_to_boolean =
+    let m = matrix [[ -1.0; 1.0; -1.0; 1.0 ]]
+    toBooleanM m |> should equal (Matrix.Generic.of_list [[ false; true; false; true ]])
+
+[<Fact>]
+let it_inverse_multiplies_matrices =
+    let m = matrix [[ -1.0; 1.0; -1.0; 1.0 ]]
+    inverseMultiply m |> should equal (matrix [[ 1.0; -1.0;  1.0; -1.0];
+                                          [-1.0;  1.0; -1.0;  1.0];
+                                          [ 1.0; -1.0;  1.0; -1.0];
+                                          [-1.0;  1.0; -1.0;  1.0]]);
+                                          
+[<Fact>]
+let it_subtracts_identity =
+    let m = matrix [[ -1.0; 1.0; -1.0; 1.0 ]] |> inverseMultiply
+    subtractIdentity m |> should equal (matrix [[ 0.0; -1.0;  1.0; -1.0];
+                                                [-1.0;  0.0; -1.0;  1.0];
+                                                [ 1.0; -1.0;  0.0; -1.0];
+                                                [-1.0;  1.0; -1.0;  0.0]]);
+
+[<Fact>]
+let it_computes_contribution =
+    matrix [[ -1.0; 1.0; -1.0; 1.0 ]] 
+        |> contribution 
+        |> should equal (matrix [[ 0.0; -1.0;  1.0; -1.0];
+                                 [-1.0;  0.0; -1.0;  1.0];
+                                 [ 1.0; -1.0;  0.0; -1.0];
+                                 [-1.0;  1.0; -1.0;  0.0]])
+    
+                                           
---
+++ Hopfield/Ops.fs
@@ -0,0 +1,30 @@
+﻿#light
+
+open Microsoft.FSharp.Math
+
+let toBipolar x = if x then 1.0 else -1.0
+
+let toBoolean x = 
+    let bipolar = (x + 1.0) / 2.0
+    if x = 1.0 then true else false
+    
+let toBipolarM m =
+    m |> Matrix.Generic.to_array2 |> Array2.map toBipolar |> Matrix.of_array2
+    
+let toBooleanM m =
+    m |> Matrix.to_array2 |> Array2.map toBoolean |> Matrix.Generic.of_array2
+    
+let inverseMultiply m = (Matrix.transpose m) * m
+    
+let subtractIdentity m = m - Matrix.identity (m.NumRows) 
+
+(*
+    I wanted:
+        let contribution = inverseMultiply >> subtractIdentity
+        
+    but when I do this, contribution is null when used from other modules,
+    like the test fixtures
+    
+*)
+
+let contribution m =  m |> (inverseMultiply >> subtractIdentity)
