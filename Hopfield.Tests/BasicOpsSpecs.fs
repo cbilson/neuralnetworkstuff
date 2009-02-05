2f33abd08fd8e4a67527b4ed7e947bae5b405254
Chris Bilson <cbilson@pobox.com>
2009-02-05 07:42:17 -0800

    more progress

--- Hopfield.Tests/BasicOpsSpecs.fs
+++ Hopfield.Tests/BasicOpsSpecs.fs
@@ -6,35 +6,15 @@ open FsxUnit.Syntax
 open Ops
 
 [<Fact>]
-let it_converts_true_to_bipolar_1 =
+let it_converts_true_to_bipolar_1() =
     toBipolar true |> should equal 1.0
 
 [<Fact>]
-let it_converts_false_to_bipolar_negative_1 =
+let it_converts_false_to_bipolar_negative_1() =
     toBipolar false |> should equal -1.0
 
 [<Fact>]
-let it_converts_1_to_boolean_true = 
-    toBoolean 1.0 |> should equal true
-    
-[<Fact>]
-let it_converts_0_to_boolean_false = 
-    toBoolean 0.0 |> should equal false
-    
-[<Fact>]
-let it_converts_boolean_matrices_to_bipolar =
-    bmatrix [[ false; true; false; true ]]
-     |> toBipolarM 
-     |> should equal (matrix [[ -1.0; 1.0; -1.0; 1.0 ]])
-
-[<Fact>]
-let it_converts_bipolar_matrices_to_boolean =
-    matrix [[ -1.0; 1.0; -1.0; 1.0 ]]
-        |> toBooleanM
-        |> should equal (bmatrix [[ false; true; false; true ]])
-
-[<Fact>]
-let it_inverse_multiplies_matrices =
+let it_inverse_multiplies_matrices() =
     matrix [[ -1.0; 1.0; -1.0; 1.0 ]]
         |> inverseMultiply 
         |> should equal (matrix [[ 1.0; -1.0;  1.0; -1.0];
@@ -43,7 +23,7 @@ let it_inverse_multiplies_matrices =
                                  [-1.0;  1.0; -1.0;  1.0]]);
                                           
 [<Fact>]
-let it_subtracts_identity =
+let it_subtracts_identity() =
     matrix [[ -1.0; 1.0; -1.0; 1.0 ]] 
         |> inverseMultiply
         |> subtractIdentity
@@ -53,7 +33,7 @@ let it_subtracts_identity =
                                  [-1.0;  1.0; -1.0;  0.0]]);
 
 [<Fact>]
-let it_computes_contribution =
+let it_computes_contribution() =
     matrix [[ -1.0; 1.0; -1.0; 1.0 ]] 
         |> contribution() 
         |> should equal (matrix [[ 0.0; -1.0;  1.0; -1.0];
@@ -72,11 +52,23 @@ let it_can_get_the_columns_of_a_matrix() =
     m |> column 2 |> should equal (matrix [ [3.0];  [7.0]; [11.0]; [15.0] ])
     m |> column 3 |> should equal (matrix [ [4.0];  [8.0]; [12.0]; [16.0] ])
 
-(*    
 [<Fact>]
-let it_can_be_presented_a_pattern =
-     browvec [ false; true; false; true ]
-        |> present
-        |> should equal (browvec [ false; false; false; false ])
+let it_can_be_presented_a_pattern() =
+     Matrix.zero 4 4
+        |> present [ false; true; false; true ]
+        |> should equal [ false; false; false; false ]
         
-*)
+[<Fact>]
+let it_can_be_trained_to_recognize_the_same_patterns_its_trained_with() =
+    let p1 = [ true; true; false; false ]
+    Matrix.zero 4 4 |> train p1 |> present p1 |> should equal p1
+        
+[<Fact>]
+let it_can_be_trained_to_recognize_patterns_simmilar_to_what_its_trained_with() =
+    let p1 = [ true; true; false; false ]
+    let p2 = [ true; false; false; false ]
+    
+    let weights = Matrix.zero 4 4 |> train p1
+    weights |> present p1 |> should equal p1
+    weights |> present p2 |> should equal p1
+        
--- Hopfield/Ops.fs
+++ Hopfield/Ops.fs
@@ -2,22 +2,8 @@
 
 open Microsoft.FSharp.Math
 
-let browvec l = RowVector.Generic.of_list l
-
-let bmatrix l = Matrix.Generic.of_list l
-
 let toBipolar x = if x then 1.0 else -1.0
 
-let toBoolean x = 
-    let bipolar = (x + 1.0) / 2.0
-    if x = 1.0 then true else false
-    
-let toBipolarM m =
-    m |> Matrix.Generic.to_array2 |> Array2.map toBipolar |> Matrix.of_array2
-    
-let toBooleanM m =
-    m |> Matrix.to_array2 |> Array2.map toBoolean |> Matrix.Generic.of_array2
-    
 let inverseMultiply (m:matrix) : matrix = (Matrix.transpose m) * m
     
 let subtractIdentity (m:matrix) : matrix = m - Matrix.identity (m.NumRows) 
@@ -25,12 +11,23 @@ let subtractIdentity (m:matrix) : matrix = m - Matrix.identity (m.NumRows)
 let contribution() =  inverseMultiply >> subtractIdentity
 
 let column i m = 
-    matrix [ [ Matrix.get m 0 i ]; 
-             [ Matrix.get m 1 i ]; 
-             [ Matrix.get m 2 i ]; 
-             [ Matrix.get m 3 i ] ]
+    matrix [ [ Matrix.get m 0 i ]; [ Matrix.get m 1 i ]; 
+             [ Matrix.get m 2 i ]; [ Matrix.get m 3 i ] ]
 
-let present (pattern:bool seq) (weights:matrix) : bool seq =
-    let n = pattern |> Seq.map toBipolar
-    seq { yield false; yield false; yield false; yield false }
+let present (pattern:bool seq) (weights:matrix) : bool list =
+    let input = seq { yield pattern |> Seq.map toBipolar } |> Matrix.of_seq
+    seq {
+        for i = 0 to (Seq.length pattern) - 1 do
+            let dp = column i weights |> Matrix.transpose |> Matrix.dot input
+            yield dp > 0.
+    } |> List.of_seq
+    
+let train (pattern:bool seq) (weights:matrix) : matrix =
+    let m2 = seq { yield pattern |> Seq.map toBipolar } |> Matrix.of_seq
+    let m1 = Matrix.transpose m2
+    let m3 = m1 * m2
+    let identity = Matrix.identity m3.NumRows
+    let m4 = m3 - identity
+    weights + m4
+    
     
