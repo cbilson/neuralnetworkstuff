17db23a493e3e576e935b5a0754970fc166c8b99
Chris Bilson <cbilson@pobox.com>
2009-02-05 13:08:32 -0800

    cleaned up the code so it _just_ repos the bug and added some comments

--- Hopfield/Ops.fs
+++ Hopfield/Ops.fs
@@ -2,29 +2,56 @@
 
 open Microsoft.FSharp.Math
 
-let toBipolar x = if x then 1.0 else -1.0
-
-let toBoolean x = 
-    let bipolar = (x + 1.0) / 2.0
-    if x = 1.0 then true else false
-    
-let toBipolarM m =
-    m |> Matrix.Generic.to_array2 |> Array2.map toBipolar |> Matrix.of_array2
-    
-let toBooleanM m =
-    m |> Matrix.to_array2 |> Array2.map toBoolean |> Matrix.Generic.of_array2
-    
 let inverseMultiply m = (Matrix.transpose m) * m
     
 let subtractIdentity m = m - Matrix.identity (m.NumRows) 
 
 (*
-    I wanted:
-        let contribution = inverseMultiply >> subtractIdentity
-        
-    but when I do this, contribution is null when used from other modules,
-    like the test fixtures
+    I want to use the code below, but I get the following exception 
+    in BasicOpsSpecs.it_computes_contribution:
     
-*)
+    ------ Test started: Assembly: Hopfield.Tests.dll ------
 
-let contribution m =  m |> (inverseMultiply >> subtractIdentity)
+Gallio TestDriven.Net Runner - Version 3.0.5 build 546
+
+Test Assemblies:
+	C:\Projects\neuralnetworkstuff\Hopfield.Tests\bin\Debug\Hopfield.Tests.dll
+
+Start time: 12:42 PM
+Verifying assembly names.
+Initializing the test runner.
+Loading the test package.
+Exploring the tests.
+Running the tests.
+Unloading the test package.
+Disposing the test runner.
+Stop time: 12:42 PM (Total execution time: 1.330 seconds)
+
+Test Report: file:///C:/Documents%20and%20Settings/cbilson/Local%20Settings/Temp/Gallio.TDNetRunner/Hopfield.Tests.dll.html
+** NO TESTS WERE RUN (No tests found) **
+TestCase 'P:BasicOpsSpecs.it_computes_contribution' failed: The type initializer for 'BasicOpsSpecs' threw an exception.
+	System.TypeInitializationException: The type initializer for 'BasicOpsSpecs' threw an exception. ---> System.TypeInitializationException: The type initializer for '<StartupCode$Hopfield.Tests>.$BasicOpsSpecs' threw an exception. ---> System.NullReferenceException: Object reference not set to an instance of an object.
+	C:\Projects\neuralnetworkstuff\Hopfield.Tests\BasicOpsSpecs.fs(9,0): at <StartupCode$Hopfield.Tests>.$BasicOpsSpecs..cctor()
+	   --- End of inner exception stack trace ---
+	at BasicOpsSpecs..cctor()
+	   --- End of inner exception stack trace ---
+	at BasicOpsSpecs.get_it_computes_contribution()
+
+
+0 passed, 1 failed, 0 skipped, took 2.20 seconds (Ad hoc).
+
+*)
+let contribution =  inverseMultiply >> subtractIdentity
+
+
+(*
+    The work-around is simple:
+    
+    let contribution() =  inverseMultiply >> subtractIdentity
+    
+    -or-
+    
+    let contribution m =  m |> (inverseMultiply >> subtractIdentity)
+    
+    But the other people in the mailing list I am on thought I should report this.
+*)
