b70e6771e941ae0ec761e3c8a0e9d0f36e26bbf0
Chris Bilson <cbilson@pobox.com>
2009-02-07 11:26:51 -0800

    made the hopfield view stuff into a user control (view) and refactored some of the XAML code

--- NeuralNetworkStuff.Visualization/Program.fs
+++ NeuralNetworkStuff.Visualization/Program.fs
@@ -2,14 +2,27 @@
 
 open System
 open System.Windows
+open System.Windows.Controls
 open XamlHelpers
+
+let mainWin =
+    let win = loadXaml<Window> "MainWindow"
+    win.Title <- "Neural Network Stuff :: Visualization"
+    win
+
+let showView n (w:Window) =
+    let va = w.FindName("viewArea") :?> ContentControl
+    let v = loadXaml<Control> n :> obj
+    va.Content <- v
+
 
 #if COMPILED
 [<STAThread>]
 do
-    let win = loadXamlWindow "MainWindow"
-    win.Title <- "Neural Network Stuff :: Visualization"
-    win.Show()
+    let w = mainWin
+    showView "HopfieldView" w
+    
+    w.Show()
 
     let app = new Application() in
         app.Run() |> ignore
--- NeuralNetworkStuff.Visualization/XamlHelpers.fs
+++ NeuralNetworkStuff.Visualization/XamlHelpers.fs
@@ -4,6 +4,7 @@ open System
 open System.IO
 open System.Reflection
 open System.Windows
+open System.Windows.Controls
 open System.Windows.Markup
 
 let getResourceName name = sprintf "%s.xaml" name
@@ -11,8 +12,9 @@ let getResourceName name = sprintf "%s.xaml" name
 let getResourceStream name = 
     Assembly.GetExecutingAssembly().GetManifestResourceStream
     <| getResourceName name
-    
-let loadXamlWindow name =
-    use stream = getResourceStream name
-    stream |> XamlReader.Load :?> Window
 
+let loadXaml<'a> name =
+    use stream = getResourceStream name
+    if null = stream then failwith (sprintf "Couldn't find a resource named %s" name)
+    stream |> XamlReader.Load :?> 'a
+    
